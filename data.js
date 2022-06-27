fc565888a8a212258fbd535209d37d35d501f4dd
Hentiofyle <92610008+Hentiofyle@users.noreply.github.com>
2022-06-27 13:20:40 +0200

    repo module 6 scrimba

---
+++ data.js
@@ -0,0 +1,20 @@
+const characterData = {
+    hero: {
+        name: "Wizard",
+        avatar: "images/wizard.png",
+        health: 60,
+        diceCount: 3,
+        currentDiceScore: []
+    },
+    monster: {
+        name: "Orc",
+        avatar: "images/orc.png",
+        health: 40,
+        diceCount: 1,
+        currentDiceScore: []
+    }
+}
+
+
+
+export  {characterData}
